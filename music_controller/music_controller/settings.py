15904c79307f5805bbb43388e2372451c197e60c
Jorgen Solum <j-solum@live.com>
2021-01-13 15:01:16 +0100

    added gitignore

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dk!x@#8hvdr)rc05js&cc#l-)1axqaq62lmghyq@0k-*9x9q3('
+SECRET_KEY = 'dk!x@#8hvdr)rc05js&cc#l-)1axqaq62lmghyq@0k-*9x9q3(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
